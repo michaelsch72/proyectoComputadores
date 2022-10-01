95f642bb3e28d51baa378adc82fe6cf3bd8716ed
michael <chalamaicol9@gmail.com>
2022-10-01 06:54:12 -0500

    inicio del proyecto

---
+++ src/main/java/com/mintic/ProyectoMintic/ProyectoMinticApplication.java
@@ -0,0 +1,13 @@
+package com.mintic.ProyectoMintic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMinticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMinticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mintic/ProyectoMintic/ProyectoMinticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintic.ProyectoMintic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMinticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
